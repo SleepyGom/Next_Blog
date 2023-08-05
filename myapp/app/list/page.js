7c1e9b4699a49b3190a6f6a9365f679b0049c863
정승일 <jungsi1217@naver.com>
2023-08-05 22:33:49 +0900

    practice making detailpage

--- myapp/app/list/page.js
+++ myapp/app/list/page.js
@@ -1,17 +1,21 @@
 import { connectDB } from "@/util/database"
+import Link from "next/link"
+import DetailLink from "./DetailLink"
 
 export default async function List () {
 
     const db = (await connectDB).db("blog")
     let result = await db.collection('post').find().toArray()
-
+    
+    console.log(result)
     return (
     <div className="list-bg">
         {
         result.map((a)=>{
             return(
             <div className="list-item">
-                <h4>{a.title}</h4>
+                <Link href={`detail/${a._id}`}><h4>{a.title}</h4></Link>
+                <DetailLink/>
                 <p>{a.content}</p>
             </div>
             )
