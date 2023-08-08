7812605de033c02dddc6f70f140bf16de5045d9a
정승일 <jungsi1217@naver.com>
2023-08-08 18:34:51 +0900

    make create

--- myapp/app/write/page.js
+++ myapp/app/write/page.js
@@ -1,9 +1,11 @@
 export default function Write (){
     return(
-        <div>
+        <div className="p-20">
             <h4>글작성</h4>
-            <form action='/api/test' method="POST">
-                <button type='submit'></button>
+            <form action='/api/post/new' method="POST">
+                <input name = 'title'/>
+                <input name = 'content'/>
+                <button type='submit'>작성</button>
             </form>
         </div>
     )
---
+++ myapp/pages/api/post/new.js
@@ -0,0 +1,13 @@
+import { connectDB } from "@/util/database";
+
+export default async function handler(request, response){
+    if(request.method == 'POST'){
+        if(request.body.title == ''){
+            return response.status(500).json('제목 쓰세요')
+        }
+        
+        const db = (await connectDB).db('blog')
+        let result = await db.collection('post').insertOne(request.body)
+        return response.status(200).redirect('/list')
+    }
+}
